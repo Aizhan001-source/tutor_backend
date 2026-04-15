0d4835efb3e215f5faf5583e603cc1848b9e72a2
ayli0106 <ayashka1608@gmail.com>
2026-04-15 13:25:08 +0500

    models new branch

--- app/data_access/db/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .role import Role
-from .tutor import Tutor
-
-__all__ = ["User", "Role", "Tutor"]
--- app/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from api.api_router import api_router
-
-app = FastAPI(title="My API")
-
-app.include_router(api_router)
